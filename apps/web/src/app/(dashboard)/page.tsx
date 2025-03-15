99256d2494cdd7108d8f87ca6ed6b61654fcc36c
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2025-03-15 00:43:23 -0300

    wip(home): Adds logout button for testing

--- apps/web/src/app/(dashboard)/page.tsx
+++ apps/web/src/app/(dashboard)/page.tsx
@@ -1,18 +1,31 @@
 "use client";
 
+import { deleteAuthToken } from "@/lib/auth";
 import { useAppControllerGetMeQuery } from "@/services/backend/endpoints";
+import { Button } from "antd";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
   const { data, isLoading } = useAppControllerGetMeQuery();
 
-  if (isLoading) {
+  if (!data || isLoading) {
     return <span>Loading...</span>;
   }
-  if (!data) {
+
+  const logout: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+
+    await deleteAuthToken("auth_token");
     router.push("/login");
-    return;
-  }
-  return <span>Hello, {data.email}</span>;
+  };
+
+  return (
+    <div>
+      <h1>{`Hello, ${data.email}`}</h1>
+      <Button type="primary" onClick={logout}>
+        Logout
+      </Button>
+    </div>
+  );
 }
--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -4,10 +4,10 @@ import { env } from "@/env";
 import { cookies } from "next/headers";
 
 export const setAuthCookie = async (name: string, token: string) => {
-  const cookieStore = await cookies();
+  const cookiesStore = await cookies();
   const encodedToken = Buffer.from(token).toString("base64");
 
-  cookieStore.set(name, encodedToken, {
+  cookiesStore.set(name, encodedToken, {
     maxAge: env.JWT_COOKIE_MAX_AGE,
     secure: env.NODE_ENV === "production",
     httpOnly: true,
@@ -15,8 +15,13 @@ export const setAuthCookie = async (name: string, token: string) => {
 };
 
 export const getAuthCookie = async (name: string) => {
-  const cookieStore = await cookies();
-  const encodedToken = cookieStore.get(name)?.value ?? "";
+  const cookiesStore = await cookies();
+  const encodedToken = cookiesStore.get(name)?.value ?? "";
 
   return Buffer.from(encodedToken, "base64").toString("utf8");
+};
+
+export const deleteAuthToken = async (name: string) => {
+  const cookiesStore = await cookies();
+  cookiesStore.delete(name);
 };
