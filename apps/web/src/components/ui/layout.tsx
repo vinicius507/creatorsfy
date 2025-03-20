766e7f18c00678587cad89679daf89eaf3698524
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2025-03-19 22:50:35 -0300

    fix(layout): Removes sidebar

--- apps/web/src/components/ui/layout.tsx
+++ apps/web/src/components/ui/layout.tsx
@@ -1,46 +1,9 @@
 "use client";
 
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import type { MenuProps } from "antd";
-import { Layout as AntLayout, Menu, theme } from "antd";
-import React from "react";
+import { Layout as AntLayout, theme } from "antd";
+import type React from "react";
 
-const { Header, Content, Footer, Sider } = AntLayout;
-
-const siderStyle: React.CSSProperties = {
-  overflow: "auto",
-  height: "100vh",
-  position: "sticky",
-  insetInlineStart: 0,
-  top: 0,
-  bottom: 0,
-  scrollbarWidth: "thin",
-  scrollbarGutter: "stable",
-};
-
-const items: MenuProps["items"] = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-  TeamOutlined,
-  ShopOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+const { Header, Content, Footer } = AntLayout;
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   const {
@@ -48,11 +11,7 @@ export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   } = theme.useToken();
 
   return (
-    <AntLayout hasSider>
-      <Sider style={siderStyle}>
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]} items={items} />
-      </Sider>
+    <AntLayout>
       <AntLayout>
         <Header style={{ padding: 0, background: colorBgContainer }} />
         <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
