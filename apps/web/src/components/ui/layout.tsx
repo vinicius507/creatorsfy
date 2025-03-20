2516b22009b3fdca3b58a26db214ac9b01980767
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2025-03-20 00:19:41 -0300

    feat(layout): Adds basic header

--- apps/web/src/components/ui/layout.tsx
+++ apps/web/src/components/ui/layout.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import { Layout as AntLayout, theme } from "antd";
-import type React from "react";
+import { logout } from "@/services/auth";
+import { useAppControllerGetMeQuery } from "@/services/backend/endpoints";
+import { useDispatch } from "@/store";
+import { LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { Layout as AntLayout, Button, Menu, theme } from "antd";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Logo } from "../logo";
 
 const { Header, Content, Footer } = AntLayout;
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const { data, isLoading } = useAppControllerGetMeQuery();
+
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
@@ -13,7 +23,34 @@ export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <AntLayout>
       <AntLayout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Header style={{ display: "flex", alignItems: "center" }}>
+          <Button type="text" style={{ color: "white" }} href="/">
+            <Logo size={20} />
+          </Button>
+          <Menu
+            selectedKeys={[]}
+            style={{ display: "flex", justifyContent: "end", flex: 1, minWidth: 0 }}
+            mode="horizontal"
+            theme="dark"
+            items={[
+              {
+                key: "user",
+                icon: React.createElement(UserOutlined),
+                label: isLoading ? "Loading..." : data?.email,
+              },
+              {
+                key: "logout",
+                icon: React.createElement(LogoutOutlined),
+                label: "Logout",
+                danger: true,
+                onClick: () => {
+                  dispatch(logout());
+                  router.push("/login");
+                },
+              },
+            ]}
+          />
+        </Header>
         <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
           <div
             style={{
