aec8bdae8f4cc203cb3e2dbe3546bd645849ea43
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2025-03-19 23:37:37 -0300

    feat(auth): Adds logout reducer

--- apps/web/src/app/(dashboard)/page.tsx
+++ apps/web/src/app/(dashboard)/page.tsx
@@ -1,29 +1,31 @@
 "use client";
 
-import { deleteAuthToken } from "@/lib/auth";
+import { logout } from "@/services/auth";
 import { useAppControllerGetMeQuery } from "@/services/backend/endpoints";
+import { useDispatch } from "@/store";
 import { Button } from "antd";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
+  const dispatch = useDispatch();
   const { data, isLoading } = useAppControllerGetMeQuery();
 
   if (!data || isLoading) {
     return <span>Loading...</span>;
   }
 
-  const logout: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+  const logoutHandler: React.MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
 
-    await deleteAuthToken("auth_token");
+    dispatch(logout());
     router.push("/login");
   };
 
   return (
     <div>
       <h1>{`Hello, ${data.email}`}</h1>
-      <Button type="primary" onClick={logout}>
+      <Button type="primary" onClick={logoutHandler}>
         Logout
       </Button>
     </div>
--- apps/web/src/services/auth/index.ts
+++ apps/web/src/services/auth/index.ts
@@ -1,16 +1,28 @@
-import { setAuthCookie } from "@/lib/auth";
+import { deleteAuthToken, setAuthCookie } from "@/lib/auth";
 import { createSlice } from "@reduxjs/toolkit";
-import { backendApi } from "../backend/endpoints";
+import { type ReadUser, backendApi } from "../backend/endpoints";
+
+type LoggedUser = {
+  user: ReadUser;
+};
+
+const initialState: Partial<LoggedUser> = {};
 
 const slice = createSlice({
   name: "auth",
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    logout: () => {
+      deleteAuthToken("auth_token");
+    },
+  },
   extraReducers: (builder) => {
     builder.addMatcher(backendApi.endpoints.authControllerLogin.matchFulfilled, (_state, { payload }) => {
       setAuthCookie("auth_token", payload.accessToken);
     });
   },
 });
+
+export const { logout } = slice.actions;
 
 export default slice.reducer;
